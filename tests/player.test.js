608289c90b5561642e0b11a0cdc333c0f48400c4
Louie Justin <louiejustinlj@gmail.com>
2025-06-03 23:05:52 +0800

    added player class and tests for player

---
+++ tests/player.test.js
@@ -0,0 +1,31 @@
+import Player from "../player.js";
+
+test("Player has its own gameboard", () => {
+  const player =  Player();
+
+  expect(player.gameboard).toBeDefined();
+})
+
+test("Computer has its own board", () => {
+  const player = Player(true)
+
+  expect(player.gameboard).toBeDefined();
+})
+
+test("Player can attack another board", () => {
+  const player = Player(false);
+  const computer = Player(true);
+
+  computer.gameboard.placeShip(0,0,3, "horizontal");
+  player.castAttack(computer.gameboard, 0,0)
+  expect(computer.gameboard.board[0][0]).toBe("hit");
+});
+
+test("Computer can attack player gameboard", () => {
+  const player = Player()
+  const computer = Player(true)
+
+  player.gameboard.placeShip(0,0,3,"horizontal");
+  computer.castAttack(player.gameboard);
+  expect(player.gameboard.board[0][0]).toBe("hit");
+})
